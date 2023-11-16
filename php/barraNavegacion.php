c0718a9e791fa1976b7b6463893175319c0d10fa
lesliesaalgado <lesliezuleimasalgadoescobar@gmail.com>
2023-11-16 14:38:28 -0600

    commit

--- php/barraNavegacion.php
+++ php/barraNavegacion.php
@@ -14,7 +14,7 @@ function generarMenu($rolUsuario) {
         </ul>';
     }elseif ($rolUsuario === "secretario") {
         echo '<ul class="nav-links">
-        <li><a href="/secretarias/muestraPacientesS.php">Pacientes</a></li>
+        <li><a href="/proyectoClinica/secretarias/muestraPacientesS.php">Pacientes</a></li>
         <li><a href="/proyectoClinica/secretarias/creacionCitasS.php">Crear cita</a></li>
         <li><a href="/proyectoClinica/secretarias/consultaCitasS.php">Consultar Citas</a></li>
         <li><button class="login-button" style="color: white;">Secretario</button></li>
