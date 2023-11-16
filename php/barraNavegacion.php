34d3669bca9f0d85c00500f601d09ec9cd97c7a4
lesliesaalgado <lesliezuleimasalgadoescobar@gmail.com>
2023-11-16 17:43:12 -0600

    button

--- php/barraNavegacion.php
+++ php/barraNavegacion.php
@@ -6,17 +6,17 @@ function generarMenu($rolUsuario) {
         echo '<li><a class="login-button" style="color: white;">Paciente</a></li>';
         echo '<li><a href="pacientes/creaCitasP.php">Crear cita</a></li>';
         echo '<li><a href="pacientes/consultaCitasP.php">Consultar citas</a></li>';
-        echo '<li><a class="login-button" style="color: white;">Paciente</a></li>';
+        
     } elseif ($rolUsuario === "admin") {
         echo '<li><a href="admin/creacionUsuarios.html">Crear usuario</a></li>';
         echo '<li><a href="admin/consultaUsuariosA.html">Usuarios</a></li>';
-        echo '<li><a class="login-button" style="color: white;">Admin</a></li>';
+        
     }
     elseif ($rolUsuario === "secretario") {
         echo '<li><a href="/secretarias/muestraPacientesS.php">Pacientes</a></li>';
         echo '<li><a href="/proyectoClinica/secretarias/creacionCitasS.php">Crear cita</a></li>';
         echo '<li><a href="/proyectoClinica/secretarias/consultaCitasS.php">Consultar Citas</a></li>';
-        echo '<li><a class="login-button" style="color: white;">Secretario</a></li>';
+       
     }
 }
 ?>
