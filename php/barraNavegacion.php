16e367c7d5f84a87af89b905867d98144e613e28
lesliesaalgado <lesliezuleimasalgadoescobar@gmail.com>
2023-11-16 17:49:34 -0600

    button

--- php/barraNavegacion.php
+++ php/barraNavegacion.php
@@ -6,15 +6,15 @@ function generarMenu($rolUsuario) {
         echo '<li><a class="login-button" style="color: white;">Paciente</a></li>';
         echo '<li><a href="pacientes/creaCitasP.php">Crear cita</a></li>';
         echo '<li><a href="pacientes/consultaCitasP.php">Consultar citas</a></li>';
-        echo '<li><a class="login-button" style="color: white;">Paciente</a></li>';
+        echo '<li><button class="login-button" style="color: white;">Paciente</button></li>';
     } elseif ($rolUsuario === "admin") {
         echo '<li><a href="admin/creacionUsuarios.html">Crear usuario</a></li>';
         echo '<li><a href="admin/consultaUsuariosA.html">Usuarios</a></li>';
-        echo '<li><a class="login-button" style="color: white;">Admin</a></li>';
+        echo '<li><button class="login-button" style="color: white;">Admin</button></li>';
     }
     elseif ($rolUsuario === "secretario") {
         echo '<li><a href="/secretarias/muestraPacientesS.php">Pacientes</a></li>';
-        echo '<li><a href="/secretarias/creacionCitasS.php">Crear cita</a></li>';
+        echo '<li><a href="/proyectoClinica/secretarias/creacionCitasS.php">Crear cita</a></li>';
         echo '<li><a href="/proyectoClinica/secretarias/consultaCitasS.php">Consultar Citas</a></li>';
         echo '<li><button class="login-button" style="color: white;">Secretario</button></li>';
     }
