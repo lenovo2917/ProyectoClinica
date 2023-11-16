6ebc90730ab88cf4d77b4830316ddfb2ba519dff
RoGeLiiOo10 <roge1086@gmail.com>
2023-11-16 12:09:53 -0600

    no se

--- php/barraNavegacion.php
+++ php/barraNavegacion.php
@@ -2,24 +2,12 @@
 function generarMenu($rolUsuario) {
     if ($rolUsuario === "paciente") {
         echo '<li><a href="pacientes/creaCitasP.html">Crear cita</a></li>';
-<<<<<<< HEAD
         echo '<li><a href="pacientes/consultaCitasP.php">Consultar citas</a></li>';
         echo '<li><a class="login-button" type="button" style="color: white;">Paciente</a></li>';
     } elseif ($rolUsuario === "admin") {
         echo '<li><a href="admin/creacionUsuarios.html">Crear usuario</a></li>';
         echo '<li><a href="admin/consultaUsuariosA.php">Usuarios</a></li>';
         echo '<li><a class="login-button" type="button" style="color: white;">Admin</a></li>';
-=======
-        echo '<li><a href="pacientes/consultaCitasP.html">Consultar citas</a></li>';
-        echo '<li><a class="login-button" style="color: white;">Paciente</a></li>';
-        echo '<li><a href="pacientes/creaCitasP.php">Crear cita</a></li>';
-        echo '<li><a href="pacientes/consultaCitasP.php">Consultar citas</a></li>';
-        echo '<li><button class="login-button" style="color: white;">Paciente</button></li>';
-    } elseif ($rolUsuario === "admin") {
-        echo '<li><a href="admin/creacionUsuarios.html">Crear usuario</a></li>';
-        echo '<li><a href="admin/consultaUsuariosA.html">Usuarios</a></li>';
-        echo '<li><button class="login-button" style="color: white;">Admin</button></li>';
->>>>>>> 16e367c7d5f84a87af89b905867d98144e613e28
     }
     elseif ($rolUsuario === "secretario") {
         echo '<li><a href="/secretarias/muestraPacientesS.php">Pacientes</a></li>';
